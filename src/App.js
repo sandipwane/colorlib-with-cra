11d4af9d2af95a8cef52d5ab7ebf143162bc3b61
Sandip Wane <swane@whitehedge.com>
2020-01-28 18:43:43 +0530

    Integrate Login Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./Login";
+
+import "./App.css";
+
+import "./css/main.css";
+import "./css/util.css";
+import "./vendor/bootstrap/css/bootstrap.css";
+import "./vendor/animate/animate.css";
+import "./vendor/css-hamburgers/hamburgers.css";
+import "./fonts/font-awesome-4.7.0/css/font-awesome.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
